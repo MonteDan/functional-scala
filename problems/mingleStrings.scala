84d6bff517fde7d34a4b2e5f3d74c0ec27ee998f
Daniel <71315420+MonteDan@users.noreply.github.com>
2024-01-01 20:52:17 +0100

    perf: faster mingle strings

--- problems/mingleStrings.scala
+++ problems/mingleStrings.scala
@@ -1,14 +1,14 @@
-object MingleStrings {
-
-  def mingle(s1: String, s2: String): String = {
-    if ( s1.length()<=1 || s2.length()<=1 ) s1 + s2
-    else s1.head.toString + s2.head.toString + mingle(s1.tail, s2.tail)
-  }
-
-  def main(args: Array[String]): Unit = {
-    val str1 = scala.io.StdIn.readLine("Enter the first string: ")
-    val str2 = scala.io.StdIn.readLine("Enter the second string: ")
-
-    println( mingle(str1, str2) )
-  }
+object Solution {
+    def main(args: Array[String]) = {
+        val str1 = scala.io.StdIn.readLine("First string:")
+        val str2 = scala.io.StdIn.readLine("Second string:")
+        println( 
+          str1
+            .zipWithIndex
+            .toList
+            .map{ 
+              case (char, index) => char.toString + str2(index).toString
+            }.reduce((acc, curr) => acc+curr)
+        )
+    }
 }
