a12dd42b51ed1311c2cc9c40404c76a1c19946c7
Daniel <71315420+MonteDan@users.noreply.github.com>
2024-01-01 21:32:23 +0100

    chore: lint

--- problems/mingleStrings.scala
+++ problems/mingleStrings.scala
@@ -1,14 +1,14 @@
-object Solution {
-    def main(args: Array[String]) = {
-        val str1 = scala.io.StdIn.readLine("First string:")
-        val str2 = scala.io.StdIn.readLine("Second string:")
-        println( 
-          str1
-            .zipWithIndex
-            .toList
-            .map{ 
-              case (char, index) => char.toString + str2(index).toString
-            }.reduce((acc, curr) => acc+curr)
-        )
-    }
+object MingleStrings {
+  def main(args: Array[String]) = {
+    val str1 = scala.io.StdIn.readLine("First string:")
+    val str2 = scala.io.StdIn.readLine("Second string:")
+    println( 
+      str1
+        .zipWithIndex
+        .toList
+        .map{ 
+          case (char, index) => char.toString + str2(index).toString
+        }.reduce((acc, curr) => acc+curr)
+    )
+  }
 }
