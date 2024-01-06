3331d998c5ff7ba472d352c7335babe94f6393bf
MonteBro <czech.illusion@gmail.com>
2024-01-07 00:50:47 +0100

    feat: shuffleArray

--- utils-src/TestUtils.scala
+++ utils-src/TestUtils.scala
@@ -1,9 +1,15 @@
 package utils
 
+import scala.reflect.ClassTag
+import scala.util.Random
+
 object TestUtils {
-  def generateRandomNumber(min: Int, max: Int): Int =
-    (new scala.util.Random).nextInt((max - min) + 1) + min
+  def generateRandomNumber(min: Int = 1, max: Int = 100): Int =
+    (new Random).nextInt((max - min) + 1) + min
 
   def generateRandomIntArray(length: Int = 10, variance: Double = 1): Array[Int] =
     Array.fill(length)(generateRandomNumber(0, (length*variance).toInt)) 
+  
+  def shuffleArray[T: ClassTag](arr: Array[T]): Array[T] = 
+    Random.shuffle(arr.toList).toArray
 }
