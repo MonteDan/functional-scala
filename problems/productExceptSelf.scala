9506fc6a46b22ef27e725192d9bb42374e550feb
MonteBro <czech.illusion@gmail.com>
2024-01-14 13:13:38 +0100

    test: Product Except Self runtime measurement

--- problems/productExceptSelf.scala
+++ problems/productExceptSelf.scala
@@ -21,13 +21,13 @@ object ProductExceptSelf {
       return result
     }
   */
-  def productExceptSelf(nums: Array[Int]): Array[Int] = {
-    def loopNums(index: Int = 0, prefix: Int = 1): Array[Int] =
+  def productExceptSelf(nums: Array[BigInt]): Array[BigInt] = {
+    def loopNums(index: Int = 0, prefix: BigInt = 1): Array[BigInt] =
       if (index == nums.length) Array()
       else
         prefix +: loopNums(index + 1, prefix * nums(index))
 
-    def mapResult(result: Array[(Int, Int)], postfix: Int = 1): Array[Int] =
+    def mapResult(result: Array[(BigInt, Int)], postfix: BigInt = 1): Array[BigInt] =
       if (result.isEmpty) Array()
       else
         mapResult(
@@ -44,19 +44,25 @@ object ProductExceptSelf {
   }
 
   def main(args: Array[String]): Unit = {
-    def test(nums: Array[Int]): Unit = {
+    def test(nums: Array[BigInt]): Unit = {
+      val (output, runtime, memory) = T.measure {
+        productExceptSelf(nums)
+      }
       printf("%-7s %-20s\n", "CASE:", nums.mkString(","))
-      printf("%-7s %-20s\n", "output:", productExceptSelf(nums).mkString(","))
+      printf("%-7s %-20s\n", "output:", output.mkString(","))
+      println(s"$runtime ms, $memory KB")
       println()
     }
 
     def testRandom(length: Int): Unit = 
-      ((length) |> (T.generateRandomIntArray(_)) |> test)
+      ((length) |> (len => T.generateRandomIntArray(len).map(BigInt(_))) |> (arr => arr.filter(_>0)) |> test)
     
 
     test(Array(1, 2, 3, 0, 5, 6))
     testRandom(5)
     testRandom(10)
     testRandom(10)
+    testRandom(30)
+    testRandom(100)
   }
 }
